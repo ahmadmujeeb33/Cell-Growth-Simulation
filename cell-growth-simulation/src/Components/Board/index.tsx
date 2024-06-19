38a393891942faadcac1066391d0050cd326469f
ahmadmujeeb33 <ahmadmujeeb333@gmail.com>
2024-06-19 14:39:50 -0400

    displaying bacteria working when cell clicked

--- cell-growth-simulation/src/Components/Board/index.tsx
+++ cell-growth-simulation/src/Components/Board/index.tsx
@@ -1,38 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FaBacterium } from "react-icons/fa6";
 
 import './style/index.css';
+
+const createInitalGrid = (size:number) => {
+    return Array.from({ length: size }, () => Array.from({ length: size }, () => false));
+}
 
 
 export const Board = () => {
 
     const size: number = 20;
-    // const rows: number[] = Array.from({ length: size }, (_, index) => index + 1);
+
+    const [grid, setGrid] = useState(()=> createInitalGrid(size))
 
 
-    const createGrid = () => {
-        return Array.from({ length: size }).map((_, rowIndex) => (
-          <tr key={rowIndex} >
-            {Array.from({ length: size }).map((_, colIndex) => (
-               
-                <td className="cell" key={colIndex}>
-                    <div className="nothing">
-                        <FaBacterium className="icon" />
-                    </div>
-                </td>
+    const toggleIcon = (rowIndex:number, colIndex:number) => {
 
-            ))}
-          </tr>
-        ));
-      };
+        setGrid(prevGrid => 
+            prevGrid.map((row, rIdx) => 
+              row.map((cell, cIdx) => (rIdx === rowIndex && cIdx === colIndex ? !cell : cell))
+            )
+        );
+    }
 
     return (
         <div className="board-container">
-                <table className="my-table">
-                    <tbody>
-                    {createGrid()}
-                    </tbody>
-                </table>
+            <table className="my-table">
+                <tbody>
+                    {grid.map((row, rowIndex) => (
+                        <tr key={rowIndex}>
+                            {row.map((cell, colIndex) => (
+                                <td
+                                    key={colIndex}
+                                    className="cell"
+                                    onClick={() => toggleIcon(rowIndex, colIndex)}
+                                >
+                                    <div className="icon-container">
+                                        {cell ?  <FaBacterium className="icon" />  : null}
+                                    </div>
+                                </td>
+                            ))}
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </div>
     )
 
